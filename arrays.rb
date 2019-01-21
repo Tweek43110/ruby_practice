a41ed0095c0e1fe7508f79e2cac37159c5548e20
Alex Coe <alex.r.coe@gmail.com>
2019-01-21 09:11:57 -0600

    Array manipulation

--- arrays.rb
+++ arrays.rb
@@ -2,11 +2,27 @@
 
 letters = ["a", "b", "c"]
 
-puts letters.length #begins at 0 index
+puts letters.length # begins at 0 index
 puts letter[0]
 
-myArray = ["Alex", true, 42]
+myArray = ["Alex", true, 42] # can contain multiple values as usual
 
 print myArray
 
 names = ["Jeff", "Tommy", "Robbert"]
+print names
+# Manipulate array content
+names << "Brian" # Shovel, adds to end of array. Much like JS arr.push()
+names[1] = "Mike" # replaces content at index 1
+
+
+# dynamic way to print out any array called
+def printArr(arr)
+    i = 0
+    while i < arr.length
+        puts arr[i]
+        i += 1
+    end
+end
+
+printArr(names)
