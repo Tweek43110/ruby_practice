e18c0997c9c599a05e3b59184534d2d04d99dd41
Alex Coe <alex.r.coe@gmail.com>
2019-01-21 08:59:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Ruby Practice
+*A collection of practice documents for Ruby, mostly from working with App Academy Open*
+
+
---
+++ arrays.rb
@@ -0,0 +1,12 @@
+#Ruby Arrays
+
+letters = ["a", "b", "c"]
+
+puts letters.length #begins at 0 index
+puts letter[0]
+
+myArray = ["Alex", true, 42]
+
+print myArray
+
+names = ["Jeff", "Tommy", "Robbert"]
